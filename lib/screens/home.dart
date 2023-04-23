991fbaec823df2ca9a3622d79add1cbd67387e01
tugulduroch <tkmusique.24@gmail.com>
2023-04-23 20:46:16 +0800

    navigation implemented

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:todo/constants/colors.dart';
 
 class name extends StatelessWidget {
   const name({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        title: Row(children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              child: Image.asset('asset/images/avatar.jpeg'),
+            ),
+          )
+        ]),
+      ),
+      body: Container(
+        child: Text('HomeScreen'),
+      ),
+    );
   }
 }
