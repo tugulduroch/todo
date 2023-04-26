4acdb37e297b404390dc4717f425c340d67e4035
tugulduroch <tkmusique.24@gmail.com>
2023-04-26 22:39:12 +0800

    item added

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:todo/constants/colors.dart';
 
+class TodoList extends StatelessWidget {
+  const TodoList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListTile(
+        onTap: () {},
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        tileColor: Colors.white,
+        leading: Icon(
+          Icons.check_box,
+          color: tdBlue,
+        ),
+        title: Text(
+          'Check Mail',
+          style: TextStyle(
+              fontSize: 16,
+              color: tdBlack,
+              decoration: TextDecoration.lineThrough),
+        ),
+      ),
+    );
+  }
+}
